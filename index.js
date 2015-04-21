fd4fc242aa7e04a237f8bd81062b17d8c2a8c81c
Gary Burgess <gary.burgess@gmail.com>
2015-04-21 02:09:38 +0100

    Fix deploy-prod, tidy stuff up a bit, use gulp-plumber

--- index.js
+++ index.js
@@ -1,205 +1,194 @@
 var fs = require("fs"),
-    child_process = require("child_process"),
-    path = require("path"),
-    _ = require("underscore"),
-    purescript = require("gulp-purescript"),
-    concat = require("gulp-concat"),
+  child_process = require("child_process"),
+  path = require("path"),
+  _ = require("underscore"),
+  purescript = require("gulp-purescript"),
+  concat = require("gulp-concat"),
+  plumber = require("gulp-plumber"),
 
-    coveralls = require("gulp-coveralls"),
-    br = require("browserify"),
-    source = require("vinyl-source-stream"),
-    buffer = require("vinyl-buffer"),
-    karma = require("karma").server;
+  coveralls = require("gulp-coveralls"),
+  browserify = require("browserify"),
+  source = require("vinyl-source-stream"),
+  buffer = require("vinyl-buffer"),
+  karma = require("karma").server,
 
-var entryFactory = require("./lib/entry-factory.js"),
-    errorHandler = require("./lib/handle-error.js"),
-    nameGetter = require("./lib/get-names.js"),
-    istanbullify = require("./lib/istanbullify.js"),
-    indir = require("./lib/indir.js");
-
+  entryFactory = require("./lib/entry-factory.js"),
+  nameGetter = require("./lib/get-names.js"),
+  istanbullify = require("./lib/istanbullify.js"),
+  indir = require("./lib/indir.js");
 
 function defineTasks(gulp, config) {
-    if (process.platform === "darwin") {
-        child_process.execSync("ulimit -n 2560");
+  if (process.platform === "darwin") {
+    child_process.execSync("ulimit -n 2560");
+  }
+  if (typeof config === 'undefined') {
+    config = defineTasks.config;
+  }
+
+  var paths = config.paths;
+
+  var getName = function (key) {
+    return key.toLowerCase().replace(".", "-");
+  };
+
+  var bundleIt = function(entry, target) {
+    return function() {
+      // console.log(entry, fs.readFileSync(entry).length);
+      return browserify({ entries: [entry] })
+        .bundle()
+        .pipe(plumber())
+        .pipe(source(target))
+        .pipe(buffer())
+        .pipe(gulp.dest(config.tmpDir));
+    };
+  };
+
+  gulp.task("link", function() {
+    var stats = fs.lstatSync("./" + config.tmpDir + "/node_modules");
+    if (stats.isSymbolicLink()) return;
+    else {
+      indir(config.tmpDir, function() {
+        fs.symlinkSync(path.resolve("./node_modules"),
+                 path.resolve("./" + config.tmpDir + "/node_modules"));
+      });
     }
-    if (typeof config === 'undefined') {
-        config = defineTasks.config;
-    }
-    var paths = config.paths,
-        bundleIt = function(entry, target) {
-            return function() {
-                var bundler = br({
-                    entries: [entry]
-                });
-                var bundle = function() {
-                    console.log("!");
-                    return bundler
-                        .bundle()
-                        .pipe(source(target))
-                        .pipe(buffer())
-                        .pipe(gulp.dest(config.tmpDir));
-            };
-                return bundle();
-            };
-        },
-        getName = function(key) {
-            return key.toLowerCase().replace(".", "-");
-        };
-    
-    gulp.task("entries", function() {
-        _(config.entries).mapObject(function(val, key) {
-            entryFactory.make(key, config.tmpDir);
-        });
-        var modules = nameGetter.modules(paths.src);
-        entryFactory.make("Test.Main", config.tmpDir, modules);
+  });
+
+  gulp.task("entry-test", function() {
+    return entryFactory.make("Test.Main", config.tmpDir, nameGetter.modules(paths.src));
+  });
+
+  gulp.task("bundle-test", ["entries", "make"], function() {
+    var pats = nameGetter.files(
+      paths.src,
+      config.tmpDir + "/node_modules"
+    );
+    return browserify({ entries: ["./" + config.tmpDir + "/" + "test-main.js"] })
+      .transform(istanbullify(pats), { global: true })
+      .bundle()
+      .pipe(plumber())
+      .pipe(source("test-main-builded.js"))
+      .pipe(buffer())
+      .pipe(gulp.dest(config.tmpDir));
+  });
+
+  _.each(config.entries, function(val, key) {
+    var name = getName(key);
+
+    gulp.task("entry-" + name, function() {
+      return entryFactory.make(key, config.tmpDir);
     });
 
-    gulp.task("docs", function() {
-        var docgen = purescript.pscDocs();
-        errorHandler(docgen);
-        return gulp.src(paths.src)
-            .pipe(docgen)
-            .pipe(gulp.dest(paths.docs.dest));
-    });
-    gulp.task("psci", function() {
-        gulp.src(
-            paths.src.concat(paths.bower).concat(paths.test)
-        ).pipe(purescript.dotPsci({}));
-    });
-    gulp.task("link", function() {
-        var stats = fs.lstatSync("./" + config.tmpDir + "/node_modules");
-        if (stats.isSymbolicLink()) return;
-        else {
-            indir(config.tmpDir, function() {
-                fs.symlinkSync(path.resolve("./node_modules"),
-                               path.resolve("./" + config.tmpDir + "/node_modules"));
-            });
-        }
+    gulp.task("prod-" + name, function() {
+      return gulp.src(_.flatten([paths.src, paths.bower]))
+        .pipe(plumber())
+        .pipe(purescript.psc({
+          output: name + "-prod.js",
+          modules: [key],
+          main: key
+        }))
+        .pipe(gulp.dest(config.tmpDir));
     });
 
-    gulp.task("make", function() {
-        var psc = purescript.pscMake({
-            output: config.tmpDir + "/node_modules"
-        });
-        errorHandler(psc);
-        return gulp.src(paths.src.concat(paths.bower).concat(paths.test))
-            .pipe(psc);
-    });
-    
+    gulp.task("bundle-" + name,
+      ["entry-" + name, "make"],
+      bundleIt("./" + config.tmpDir + "/" + name + ".js", name + "-builded.js"));
 
-    _(config.entries).mapObject(function(val, key) {
-        var name = getName(key);
-        gulp.task(
-            "prod-" + getName(key),
-            function() {
-                var psc = purescript.psc({
-                    output: getName(key) + "-prod.js",
-                    modules: [key],
-                    main: key
-                });
-                errorHandler(psc);
-                return gulp.src(paths.src.concat(paths.bower))
-                    .pipe(psc)
-                    .pipe(gulp.dest(config.tmpDir));
-            });
-        
-        gulp.task(
-            "bundle-" +  getName(key),
-            ["entries", "make"],
-            bundleIt("./" + config.tmpDir + "/" + name + ".js",
-                     getName(key) + "-builded.js"));
+    gulp.task("bundle-prod-" + name,
+      ["entry-" + name, "prod-" + name],
+      bundleIt("./" + config.tmpDir + "/" + name + "-prod.js", name + "-prod-builded.js"));
 
-        gulp.task(
-            "bundle-prod-" + getName(key),
-            ["entries", "prod-" + getName(key)],
-            bundleIt("./" + config.tmpDir + "/" + name + "-prod.js",
-                     getName(key) + "-prod-builded.js"));
-        
-        gulp.task(
-            "deploy-" + name,
-            ["entries", "bundle-" + name],
-            function() {
-                gulp.src(["./" + config.tmpDir + "/" + name + "-builded.js"])
-                    .pipe(concat(val.name + ".js"))
-                    .pipe(gulp.dest(val.dir));
-            });
-        
-        gulp.task(
-            "deploy-prod-" + name,
-            ["entries", "bundle-prod-" + name],
-            function() {
-                gulp.src(["./" + config.tmpDir + "/" + name + "-prod.js"])
-                    .pipe(concat(val.name + ".js"))
-                    .pipe(gulp.dest(val.dir));
-            });
-        gulp.task(
-            "watch-" + name,
-            ["deploy-" + name],
-            function() {
-                gulp.watch(
-                    paths.src.concat(paths.test),
-                    ["deploy-" + name]
-                );
-            });
+    gulp.task("deploy-" + name,
+      ["entry-" + name, "bundle-" + name],
+      function() {
+        return gulp.src(["./" + config.tmpDir + "/" + name + "-builded.js"])
+          .pipe(plumber())
+          .pipe(concat(val.name + ".js"))
+          .pipe(gulp.dest(val.dir));
+      });
 
-    });
+    gulp.task("deploy-prod-" + name,
+      ["bundle-prod-" + name],
+      function() {
+        console.log("fuoy", name);
+        return gulp.src(["./" + config.tmpDir + "/" + name + "-prod.js"])
+          .pipe(plumber())
+          .pipe(concat(val.name + ".js"))
+          .pipe(gulp.dest(val.dir));
+      });
 
-    gulp.task("compile", _(config.entries).map(function(val, key) {
-        return "prod-" + getName(key);     
-    }));
-    
-    gulp.task("bundle-prod", ["entries", "compile"], function() {
-        _(config.entries).mapObject(function(val, key) {
-            bundleIt("./" + config.tmpDir + "/" + getName(key) + "-prod.js",
-                     getName(key) + "-prod-builded.js")();
-        });
-    });
+    gulp.task("watch-" + name,
+      ["deploy-" + name],
+      function() {
+        gulp.watch(_.flatten([paths.src, paths.test]), ["deploy-" + name]);
+      });
 
-    gulp.task("deploy-prod", ["bundle-prod"], function() {
-        _(config.entries).mapObject(function(val, key) {
-            gulp.src(["./" + config.tmpDir + "/" + getName(key) + "-prod-builded.js"])
-                .pipe(concat(val.name + ".js"))
-                .pipe(gulp.dest(val.dir));
-        });
-    });
-    gulp.task("bundle-test", ["entries", "make"], function() {
-        var bundler = br({
-            entries: ["./" + config.tmpDir + "/" + "test-main.js"]
-        });
-        var pats = nameGetter.files(
-            paths.src,
-            config.tmpDir + "/node_modules"
-        );
-        var bundle = function() {
-            return bundler
-                .transform(istanbullify(pats), {global: true})
-                .bundle()
-                .pipe(source("test-main-builded.js"))
-                .pipe(buffer())
-                .pipe(gulp.dest(config.tmpDir));
-        };
-        return bundle();
-    });
+  });
 
-    gulp.task("karma", ["bundle-test"], function(cb) {
-        karma.start({
-            configFile: __dirname + "/karma.conf.js",
-            action: "run",
-            singleRun: true
-        }, cb);
-    });
-    gulp.task("cover", ["karma"], function() {
-        gulp.src("../../coverage/**/lcov.info")
-            .pipe(coveralls());
-    });
+  var entryNames = _.keys(config.entries).map(getName);
 
-    
-    gulp.task("watch-test", ["bundle-test"], function() {
-        gulp.watch(
-            paths.src.concat(paths.test),
-            ["bundle-test"]
-        );
-    });
+  gulp.task("make", function() {
+    return gulp.src(_.flatten([paths.src, paths.bower, paths.test]))
+      .pipe(plumber())
+      .pipe(purescript.pscMake({
+        output: config.tmpDir + "/node_modules"
+      }));
+  });
+
+  gulp.task("entries", ["entry-test"].concat(entryNames.map(function (name) {
+    return "entry-" + name;
+  })));
+
+  gulp.task("psci", function() {
+    return gulp.src(_.flatten([paths.src, paths.bower, paths.test]))
+      .pipe(plumber())
+      .pipe(purescript.dotPsci({}));
+  });
+
+  gulp.task("docs", function() {
+    return gulp.src(paths.src)
+      .pipe(plumber())
+      .pipe(purescript.pscDocs())
+      .pipe(gulp.dest(paths.docs.dest));
+  });
+
+  gulp.task("prod", entryNames.map(function (name) {
+    return "prod-" + name;
+  }));
+
+  gulp.task("bundle-", entryNames.map(function (name) {
+    return "bundle-" + name;
+  }));
+
+  gulp.task("bundle-prod", entryNames.map(function (name) {
+    return "bundle-prod-" + name;
+  }));
+
+  gulp.task("deploy", entryNames.map(function (name) {
+    return "deploy-" + name;
+  }));
+
+  gulp.task("deploy-prod", entryNames.map(function (name) {
+    return "deploy-prod-" + name;
+  }));
+
+  gulp.task("karma", ["bundle-test"], function(cb) {
+    karma.start({
+      configFile: __dirname + "/karma.conf.js",
+      action: "run",
+      singleRun: true
+    }, cb);
+  });
+
+  gulp.task("cover", ["karma"], function() {
+    gulp.src("../../coverage/**/lcov.info")
+      .pipe(plumber())
+      .pipe(coveralls());
+  });
+
+  gulp.task("watch-test", ["bundle-test"], function() {
+    gulp.watch(_.flatten([paths.src, paths.test]), ["bundle-test"]);
+  });
 }
 
 var defaultConfig = {
@@ -216,8 +205,6 @@ var defaultConfig = {
   },
   tmpDir: 'dist'
 };
-
-
 
 module.exports = defineTasks;
 module.exports.config = defaultConfig;
